c02bb379c83d4571b12a4a2bf7a92ebbc6d7296c
jhemming93 <jhemming93@outlook.com>
2023-08-29 11:44:34 -0600

    home updated

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,6 +1,25 @@
 </main>
-<footer>
-<?= view_cell("MainNavMenu::show"); ?>
+<footer class="bg-custom-ivory p-2">
+    <div class="container-xl">
+    
+    <div class="justify-content-end">
+        <h4 style="padding-left: 16px;">Site Navigation</h4>
+<ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link text-dark" aria-current="page" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="/portfolios">Portfolio Gallery</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="/events">Events</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="/contact">Contact Us</a>
+      </li>
+</ul>
+
+</div>
 </footer>
 </body>
 </html>
