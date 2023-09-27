36cdfc1739c1bab598773435347b69f01d7ec921
jhemming93 <jhemming93@outlook.com>
2023-09-26 21:52:58 -0600

    Update Contact.php

--- app/Controllers/Contact.php
+++ app/Controllers/Contact.php
@@ -34,7 +34,7 @@ class Contact extends BaseController
                 $message = $request->getPost('message');
 
                 $email->setFrom($myemail, $name);
-                $email->setTo('justicehemming@gmail.com');
+                $email->setTo('yegjdev@gmail.com');
 
                 $email->setSubject($subject);
                 $email->setMessage($message);
