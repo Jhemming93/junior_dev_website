bf809808c4c48091dcd7d9fbfd46b8f8797cb2ba
jhemming93 <jhemming93@outlook.com>
2023-09-28 11:27:51 -0600

    Update contact-result.php

--- app/Views/pages/contact-result.php
+++ app/Views/pages/contact-result.php
@@ -1,13 +1,13 @@
 <?php $result = 'Success' ?>
-<div class="d-flex justify-content-center align-items-center">
+<div class="d-flex justify-content-center align-items-center" style="height:100%;">
     <?php if(isset($result)): ?>
         <div>
         <?php if($result= 'Success'): ?>
-                <h3>Your Message was successfully sent.</h3>
-                <p>We will get back to you as soon as possible.</p>     
+                <h3 class="text-center">Your Message was successfully sent.</h3>
+                <p class="text-center">We will get back to you as soon as possible.</p>     
         <?php elseif($result = 'Failed'): ?>
-                <h3>Oh No! Your messaged failed.</h3>
-                <p>We have been notfied and will work to fix this issue.</p>   
+                <h3 class="text-center">Oh No! Your messaged failed.</h3>
+                <p class="text-center">We have been notfied and will work to fix this issue.</p>   
         <?php endif; ?> 
         </div>
     <?php endif; ?>
