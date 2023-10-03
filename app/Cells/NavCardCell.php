d444b5002aaf716c7c82bd03ad6de9bcfc971afa
jhemming93 <jhemming93@outlook.com>
2023-10-02 21:45:28 -0600

    nav card

--- app/Cells/NavCardCell.php
+++ app/Cells/NavCardCell.php
@@ -10,7 +10,27 @@ class NavCardCell extends Cell
      public $image;
     public $description;
     public $title;
-    public $link;   
+    public $link;
+
+    public function mount()
+    {
+        $this->computed = sprintf('%s - %s', $this->image, $this->description, $this->title, $this->link);
+    }
+
+    // public function getComputedProperty(): string
+    // {
+    //     return $this->computed;
+    // }
+
+    // public function getTypeProperty(): string
+    // {
+    //     return $this->type;
+    // }
+
+    // public function getMessageProperty(): string
+    // {
+    //     return $this->message;
+    // }   
 
     
 }
